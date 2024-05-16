ded42752696f20da123ab178329f96fe04c1bd55
theodesic <kimanifekix54@gmail.com>
2024-05-16 12:12:07 +0300

    new changes

--- index.js
+++ index.js
@@ -62,3 +62,59 @@ let close = document.getElementById("close");
 close.addEventListener("click", function(){
   document.querySelector(".main").style.display = "block";
 })
+
+
+//form validation
+
+let form = document.getElementById("registration");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let confirmPassword = document.getElementById("confirmPassword");
+let submit = document.getElementById("submit");
+
+
+let emailError = document.getElementById("emailError");
+let passwordError = document.getElementById("passwordError");
+let confirmPasswordError = document.getElementById("confirmPasswordError");
+
+
+let emailCheck = /^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$/gm
+
+
+form.addEventListener("submit", (e)=>{
+    // if (username.value === "" || username.value === null){
+    //     e.preventDefault();
+    //     usernameError.innerHTML = "username required";
+    // }
+    if (email.value === "" || email.value === null){
+        e.preventDefault();
+        emailError.innerHTML = "email required";
+    }else if(!email.value.match(emailCheck)){
+        e.preventDefault();
+        emailError.innerHTML = "invalid email address";
+    }
+    if (password.value === "" || password.value === null){
+        e.preventDefault();
+        passwordError.innerHTML = "password required";
+    }else if(password.value.length <=5){
+        e.preventDefault();
+        passwordError.innerHTML = "password must be more than 6 caracters";
+    }else if(password.value.length >20){
+        e.preventDefault();
+        passwordError.innerHTML = "passwprd must be less than 20 characters";
+    }else if(password.value !== confirmPassword.value){
+        confirmPasswordError.innerHTML = "password do not match"
+    }
+})
+
+/*form.addEventListener("mouseover", function(event){
+    event.target.style.background = "green";
+    event.target.style.color = "bisque";
+    event.target.style.transition = "3s";
+})*/
+
+form.addEventListener("mouseout", function(event){
+    event.target.style.transition = "2s";
+})
+
+
